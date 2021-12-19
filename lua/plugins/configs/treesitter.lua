8d8721611744d08e2c220bc356d6998ee39dd041
Laughing-q <1185102784@qq.com>
2021-12-19 21:18:12 +0800

    update treesitter config

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,48 +1,12 @@
 local treesitter_opts = {
-	ensure_installed = {}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = { "python", "c", "cpp", "cuda", "dockerfile", "lua", "vim", "bash", "yaml", "bibtex", "latex" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	ignore_install = {},
-	matchup = {
-		enable = false, -- mandatory, false will disable the whole extension
-		-- disable = { "c", "ruby" },  -- optional, list of language that will be disabled
-	},
 	highlight = {
 		enable = true, -- false will disable the whole extension
 		additional_vim_regex_highlighting = true,
 		use_languagetree = true,
-		disable = { "latex" },
+		-- disable = { "latex" },
 	},
-	context_commentstring = {
-		enable = false,
-		config = { css = "// %s" },
-	},
-	-- indent = {enable = true, disable = {"python", "html", "javascript"}},
-	-- TODO seems to be broken
-	indent = { enable = false, disable = { "yaml" } },
-	autotag = { enable = false },
-	textobjects = {
-		swap = {
-			enable = false,
-			-- swap_next = textobj_swap_keymaps,
-		},
-		-- move = textobj_move_keymaps,
-		select = {
-			enable = false,
-			-- keymaps = textobj_sel_keymaps,
-		},
-	},
-	textsubjects = {
-		enable = false,
-		keymaps = { ["."] = "textsubjects-smart", [";"] = "textsubjects-big" },
-	},
-	rainbow = {
-		enable = false,
-		extended_mode = true, -- Highlight also non-parentheses delimiters, boolean or table: lang -> boolean
-		max_file_lines = 1000, -- Do not enable for files with more than 1000 lines, int
-	},
-	-- refactor = {
-	--   highlight_definitions = { enable = true },
-	--   highlight_current_scope = { enable = true },
-	-- },
 }
 
 local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
