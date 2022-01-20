1ceedf6e43b40e7e1ed32bce80ffdb2778fd834b
Laughing-q <1185102784@qq.com>
2022-01-20 15:32:43 +0800

    add distant.nvim

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -521,4 +521,20 @@ return packer.startup(function()
 	-- use {
 	--    "xuhdev/vim-latex-live-preview",
 	-- }
+  
+  use {
+    'chipsenkbeil/distant.nvim',
+		cmd = "DistantLaunch",
+    -- DistantLaunch ip mode=ssh ssh.user=username
+    config = function()
+      require('distant').setup {
+        -- Applies Chip's personal settings to every machine you connect to
+        --
+        -- 1. Ensures that distant servers terminate with no connections
+        -- 2. Provides navigation bindings for remote directories
+        -- 3. Provides keybinding to jump into a remote file's parent directory
+        ['*'] = require('distant.settings').chip_default()
+      }
+    end
+  }
 end)
