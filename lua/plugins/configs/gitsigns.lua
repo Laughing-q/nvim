c6cbdabf9a84cd39ec2c31f80e367390b5794dc2
Laughing-q <1185102784@qq.com>
2022-01-27 22:58:29 +0800

    update

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -43,7 +43,10 @@ local opts = {
 		noremap = true,
 		buffer = true,
 	},
-	watch_index = { interval = 1000 },
+	watch_gitdir = {
+		interval = 1000,
+		follow_files = true,
+	},
 	sign_priority = 6,
 	update_debounce = 200,
 	status_formatter = nil, -- Use default
