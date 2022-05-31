1c2a915677fb1561c35d46e63cb9747083aa427a
Laughing-q <1185102784@qq.com>
2022-05-31 17:32:18 +0800

    cancel pyright autoimport

--- lua/plugins/configs/lsp/settings/pyright.lua
+++ lua/plugins/configs/lsp/settings/pyright.lua
@@ -1,8 +1,10 @@
 return {
+  -- more settings see https://github.com/microsoft/pyright/blob/main/packages/vscode-pyright/package.json
   settings = {
     python = {
       analysis = {
         autoSearchPaths = true,
+        autoImportCompletions = false,
         diagnosticMode = "workspace",
         useLibraryCodeForTypes = true,
         typeCheckingMode = "off",
