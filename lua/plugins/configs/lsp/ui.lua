9bdb2ab58932658efd49e9199863e3df8d9fa96d
Laughing-q <1185102784@qq.com>
2022-10-08 16:49:24 +0800

    update lsp

--- lua/plugins/configs/lsp/ui.lua
+++ lua/plugins/configs/lsp/ui.lua
@@ -1,56 +1,55 @@
 -- replace the default lsp diagnostic symbols
 local signs = { Error = " ", Warn = " ", Hint = " ", Info = " " }
 for type, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. type
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+	local hl = "DiagnosticSign" .. type
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
 end
 
 -- Customizing how diagnostics are displayed
 local config = {
-  -- disable virtual text
-  virtual_text = false,
-  -- virtual_text = {
-  --    prefix = "",
-  --    spacing = 0,
-  -- },
-  -- show signs
-  signs = {
-    active = signs,
-  },
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
+	-- disable virtual text
+	virtual_text = false,
+	-- virtual_text = {
+	--    prefix = "",
+	--    spacing = 0,
+	-- },
+	-- show signs
+	signs = {
+		active = signs,
+	},
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 }
 vim.diagnostic.config(config)
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-  border = "single",
+	border = "single",
 })
--- vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
--- 	border = "single",
--- })
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(require("lsp_signature").signature_handler, {
-  border = "single",
+vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+	focusable = true,
+	style = "minimal",
+	border = "rounded",
 })
 
 -- suppress error messages from lang servers
 vim.notify = function(msg, log_level, _opts)
-  if msg:match("exit code") then
-    return
-  end
-  if log_level == vim.log.levels.ERROR then
-    vim.api.nvim_err_writeln(msg)
-  else
-    vim.api.nvim_echo({ { msg } }, true, {})
-  end
+	if msg:match("exit code") then
+		return
+	end
+	if log_level == vim.log.levels.ERROR then
+		vim.api.nvim_err_writeln(msg)
+	else
+		vim.api.nvim_echo({ { msg } }, true, {})
+	end
 end
 
 -- show line diagnostics automatically in hover window
