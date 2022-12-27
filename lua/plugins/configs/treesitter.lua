d0d60c5ce24e925dad45bc999834d17547dd8b0d
Laughing-q <1185102784@qq.com>
2022-12-27 19:44:57 +0800

    update

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -45,10 +45,10 @@ local treesitter_opts = {
     swap = {
       enable = true,
       swap_next = {
-        ['<leader>a'] = '@parameter.inner',
+        ['<leader>s'] = '@parameter.inner',
       },
       swap_previous = {
-        ['<leader>A'] = '@parameter.inner',
+        ['<leader>S'] = '@parameter.inner',
       },
     },
   },
