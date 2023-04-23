0f3584bc984566354a95c5e612d21ae87577d953
Laughing-q <1185102784@qq.com>
2023-04-23 16:22:58 +0800

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -71,6 +71,8 @@ require("lazy").setup({
 	{ -- Additional text objects via treesitter
 		"nvim-treesitter/nvim-treesitter-textobjects",
 		dependencies = { "nvim-treesitter" },
+		enabled = status.treesitter,
+		event = "BufRead",
 	},
 
 	-- git stuff
