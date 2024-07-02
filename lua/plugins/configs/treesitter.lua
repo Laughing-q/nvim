3a78472de8d766adee50996759e2b49102768841
Laughing-q <1185102784@qq.com>
2024-07-02 19:01:27 +0800

    format

--- lua/plugins/configs/treesitter.lua
+++ lua/plugins/configs/treesitter.lua
@@ -1,5 +1,21 @@
 local treesitter_opts = {
-	ensure_installed = { "python", "c", "cpp", "cmake", "make", "cuda", "dockerfile", "lua", "vim", "bash", "yaml", "bibtex", "latex", "markdown" }, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
+	ensure_installed = {
+		"python",
+		"c",
+		"cpp",
+		"cmake",
+		"make",
+		"cuda",
+		"dockerfile",
+		"lua",
+		"vim",
+		"vimdoc",
+		"bash",
+		"yaml",
+		"bibtex",
+		"latex",
+		"markdown",
+	}, -- one of "all", "maintained" (parsers with maintainers), or a list of languages
 	ignore_install = {},
 	highlight = {
 		enable = true, -- false will disable the whole extension
@@ -8,50 +24,50 @@ local treesitter_opts = {
 		-- disable = { "latex" },
 	},
 
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['aa'] = '@parameter.outer',
-        ['ha'] = '@parameter.inner',
-        ['af'] = '@function.outer',
-        ['hf'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['hc'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']]'] = '@function.outer',
-        [']c'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']['] = '@function.outer',
-        [']C'] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[['] = '@function.outer',
-        ['[c'] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[]'] = '@function.outer',
-        ['[C'] = '@class.outer',
-      },
-    },
-    swap = {
-      enable = true,
-      swap_next = {
-        ['<leader>s'] = '@parameter.inner',
-      },
-      swap_previous = {
-        ['<leader>S'] = '@parameter.inner',
-      },
-    },
-  },
+	textobjects = {
+		select = {
+			enable = true,
+			lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+			keymaps = {
+				-- You can use the capture groups defined in textobjects.scm
+				["aa"] = "@parameter.outer",
+				["ha"] = "@parameter.inner",
+				["af"] = "@function.outer",
+				["hf"] = "@function.inner",
+				["ac"] = "@class.outer",
+				["hc"] = "@class.inner",
+			},
+		},
+		move = {
+			enable = true,
+			set_jumps = true, -- whether to set jumps in the jumplist
+			goto_next_start = {
+				["]]"] = "@function.outer",
+				["]c"] = "@class.outer",
+			},
+			goto_next_end = {
+				["]["] = "@function.outer",
+				["]C"] = "@class.outer",
+			},
+			goto_previous_start = {
+				["[["] = "@function.outer",
+				["[c"] = "@class.outer",
+			},
+			goto_previous_end = {
+				["[]"] = "@function.outer",
+				["[C"] = "@class.outer",
+			},
+		},
+		swap = {
+			enable = true,
+			swap_next = {
+				["<leader>s"] = "@parameter.inner",
+			},
+			swap_previous = {
+				["<leader>S"] = "@parameter.inner",
+			},
+		},
+	},
 }
 
 local status_ok, treesitter_configs = pcall(require, "nvim-treesitter.configs")
