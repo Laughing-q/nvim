7c1eeccbd0b0939fcfba58277223c41300f99a93
Laughing-q <1185102784@qq.com>
2024-07-10 18:08:58 +0800

    update tokyonight config

--- lua/lq/plugins/colorscheme.lua
+++ lua/lq/plugins/colorscheme.lua
@@ -2,6 +2,27 @@ return {
 	{
 		"folke/tokyonight.nvim",
 		enabled = false,
+		config = function()
+			require("tokyonight").setup({
+				style = "storm",
+				transparent = false,
+				terminal_colors = true,
+				styles = {
+					sidebars = "transparent",
+					floats = "transparent",
+				},
+				sidebars = { "qf", "help" },
+
+				on_highlights = function(hl, c)
+					hl.LineNr = {
+						fg = "#bb9af7",
+					}
+					hl.BufferLineFill = {
+						bg = c.black,
+					}
+				end,
+			})
+		end,
 	},
 	{
 		"rebelot/kanagawa.nvim",
