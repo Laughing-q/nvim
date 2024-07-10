9dfa66cc240001363be297c2e3adfa83124b95c5
Laughing-q <1185102784@qq.com>
2024-07-10 15:42:27 +0800

    update gitsign

--- lua/plugins/configs/gitsigns.lua
+++ lua/plugins/configs/gitsigns.lua
@@ -12,6 +12,14 @@ local opts = {
 		changedelete = { text = "~" },
 		untracked = { text = "┆" },
 	},
+	signs_staged = {
+		add = { text = "┃" },
+		change = { text = "┃" },
+		delete = { text = "_" },
+		topdelete = { text = "‾" },
+		changedelete = { text = "~" },
+		untracked = { text = "┆" },
+	},
 	-- signs = {
 	-- 	add = {
 	-- 		text = "+",
@@ -31,6 +39,42 @@ local opts = {
 	sign_priority = 6,
 	update_debounce = 200,
 	status_formatter = nil, -- Use default
+
+	on_attach = function(bufnr)
+		local function map(mode, l, r, opts)
+			opts = opts or {}
+			opts.buffer = bufnr
+			vim.keymap.set(mode, l, r, opts)
+		end
+
+		-- Navigation
+		map("n", "]g", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "]c", bang = true })
+			else
+				gitsigns.nav_hunk("next")
+			end
+		end)
+
+		map("n", "[g", function()
+			if vim.wo.diff then
+				vim.cmd.normal({ "[c", bang = true })
+			else
+				gitsigns.nav_hunk("prev")
+			end
+		end)
+
+		-- Actions
+		map("n", "<leader>gl", gitsigns.blame_line, { desc = "Git Blame" })
+		map("n", "<leader>gj", gitsigns.toggle_linehl, { desc = "Toggle Line Highlight" })
+		map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Git Preview Hunk" })
+
+		map("n", "<leader>gr", gitsigns.reset_hunk, { desc = "git reset hunk" })
+		map("n", "<leader>gr", gitsigns.reset_buffer, { desc = "git reset buffer" })
+		map("n", "<leader>gs", gitsigns.stage_hunk, { desc = "git stage hunk" })
+		map("n", "<leader>gu", gitsigns.undo_stage_hunk, { desc = "git undo stage" })
+		map("n", "<leader>gd", gitsigns.diffthis, { desc = "git diff" })
+	end,
 }
 
 gitsigns.setup(opts)
