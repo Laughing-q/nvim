e03fd5df6ccba44ce28ce92d8746eb327cda707a
Laughing-q <1185102784@qq.com>
2024-07-10 14:09:26 +0800

    update finders

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -94,10 +94,6 @@ telescope.setup({
 			case_mode = "smart_case", -- or "ignore_case" or "respect_case"
 			-- the default case_mode is "smart_case"
 		},
-		media_files = {
-			filetypes = { "png", "webp", "jpg", "jpeg" },
-			find_cmd = "rg", -- find command (defaults to `fd`)
-		},
 	},
 })
 
