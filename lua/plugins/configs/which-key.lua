b4630837ab6bcc3cfea6a736a5dd4eb7f1f2f4b6
Laughing-q <1185102784@qq.com>
2024-07-10 15:42:32 +0800

    clean which-key

--- lua/plugins/configs/which-key.lua
+++ lua/plugins/configs/which-key.lua
@@ -88,28 +88,12 @@ leader_key = {
 
 		g = {
 			name = "Git",
-			k = { "<cmd>lua require 'gitsigns'.next_hunk()<cr>", "Next Hunk" },
-			i = { "<cmd>lua require 'gitsigns'.prev_hunk()<cr>", "Prev Hunk" },
-			l = { "<cmd>lua require 'gitsigns'.blame_line()<cr>", "Blame" },
-			j = { "<cmd>Gitsigns toggle_linehl<cr>", "Checkout commit" },
-			p = { "<cmd>lua require 'gitsigns'.preview_hunk()<cr>", "Preview Hunk" },
-			r = { "<cmd>lua require 'gitsigns'.reset_hunk()<cr>", "Reset Hunk" },
-			R = { "<cmd>lua require 'gitsigns'.reset_buffer()<cr>", "Reset Buffer" },
-			s = { "<cmd>lua require 'gitsigns'.stage_hunk()<cr>", "Stage Hunk" },
-			u = {
-				"<cmd>lua require 'gitsigns'.undo_stage_hunk()<cr>",
-				"Undo Stage Hunk",
-			},
 			o = { "<cmd>Telescope git_status<cr>", "Open changed file" },
 			b = { "<cmd>Telescope git_branches<cr>", "Checkout branch" },
 			c = { "<cmd>Telescope git_commits<cr>", "Checkout commit" },
 			C = {
 				"<cmd>Telescope git_bcommits<cr>",
 				"Checkout commit(for current file)",
-			},
-			d = {
-				"<cmd>Gitsigns diffthis HEAD<cr>",
-				"Git Diff",
 			},
 		},
 
