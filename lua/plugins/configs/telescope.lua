d624b22cf6b02ace9ebb63ff5cb2cefeb54bb3e2
Laughing-q <1185102784@qq.com>
2024-07-10 15:04:53 +0800

    update telescope

--- lua/plugins/configs/telescope.lua
+++ lua/plugins/configs/telescope.lua
@@ -3,10 +3,7 @@ if not present then
 	return
 end
 
-local status_ok, actions = pcall(require, "telescope.actions")
-if not status_ok then
-	return
-end
+local actions = require("telescope.actions")
 local action_layout = require("telescope.actions.layout")
 
 telescope.setup({
@@ -27,25 +24,18 @@ telescope.setup({
 		selection_strategy = "reset",
 		-- sorting_strategy = "descending",
 		sorting_strategy = "ascending",
-		layout_strategy = "horizontal",
+		layout_strategy = "vertical",
 		layout_config = {
 			horizontal = {
 				prompt_position = "top",
         mirror = true,
-				preview_width = 0.6,
+				preview_width = 0.5,
 				-- results_width = 0.8,
 			},
 			vertical = {
 				prompt_position = "top",
-				mirror = false,
+				mirror = true,
 			},
-			-- center = {
-			-- 	prompt_position = "bottom",
-			-- 	mirror = false,
-			-- },
-			-- width = 0.87,
-			-- height = 0.80,
-			-- preview_cutoff = 120,
 		},
 		file_sorter = require("telescope.sorters").get_fuzzy_file,
 		file_ignore_patterns = { "node_modules" },
@@ -71,13 +61,13 @@ telescope.setup({
 				["<C-n>"] = actions.cycle_history_next,
 				["<C-p>"] = actions.cycle_history_prev,
 				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-        ["<M-l>"] = action_layout.toggle_preview,
+				["<M-l>"] = action_layout.toggle_preview,
 			},
 			n = {
 				["<C-n>"] = actions.move_selection_next,
 				["<C-p>"] = actions.move_selection_previous,
 				["<C-q>"] = actions.smart_send_to_qflist + actions.open_qflist,
-        ["<M-l>"] = action_layout.toggle_preview,
+				["<M-l>"] = action_layout.toggle_preview,
 
 				["k"] = actions.move_selection_next,
 				["i"] = actions.move_selection_previous,
@@ -97,17 +87,45 @@ telescope.setup({
 	},
 })
 
-local extensions = { "fzf", "harpoon" }
--- local packer_repos = [["extensions", "telescope-fzf-native.nvim"]]
+pcall(require("telescope").load_extension, "fzf")
+pcall(require("telescope").load_extension, "harpoon")
 
--- telescope media_files
--- if vim.fn.executable "ueberzug" == 1 then
---    table.insert(extensions, "media_files")
---    packer_repos = packer_repos .. ', "telescope-media-files.nvim"'
--- end
+local builtin = require("telescope.builtin")
 
--- pcall(function()
-for _, ext in ipairs(extensions) do
-	telescope.load_extension(ext)
-end
--- end)
+vim.keymap.set("n", "<leader>ff", function()
+	builtin.find_files(require("telescope.themes").get_dropdown({ layout_config = { width = 0.5 }, previewer = false }))
+end, { desc = "Find File" })
+
+vim.keymap.set("n", "<leader>fo", function()
+	builtin.oldfiles(
+		require("telescope.themes").get_dropdown({ layout_config = { height = 0.6, width = 0.6 }, previewer = false })
+	)
+end, { desc = "Find Old File" })
+
+vim.keymap.set("n", "<leader>fs", function()
+	builtin.spell_suggest(
+		require("telescope.themes").get_cursor({ layout_config = { height = 0.4, width = 0.5 }, previewer = false })
+	)
+end, { desc = "Find Spell" })
+
+vim.keymap.set("n", "<leader>fn", function()
+	builtin.current_buffer_fuzzy_find(
+		require("telescope.themes").get_dropdown({ layout_config = { height = 0.6, width = 0.7 }, previewer = false })
+	)
+end, { desc = "Find Word in current buffer" })
+
+vim.keymap.set("n", "<leader>fa", function()
+	builtin.find_files({ width = 0.5, previewer = false, hidden = true })
+end, { desc = "Find Hidden File" })
+
+vim.keymap.set("n", "<leader>fj", builtin.grep_string, { desc = "Find Word under the cursor" })
+vim.keymap.set("n", "<leader>fw", builtin.live_grep, { desc = "Find Word" })
+vim.keymap.set("n", "<leader>fh", builtin.help_tags, { desc = "Find Help" })
+vim.keymap.set("n", "<leader>fr", builtin.registers, { desc = "Find Registers" })
+vim.keymap.set("n", "<leader>fk", builtin.keymaps, { desc = "Find Keymaps" })
+vim.keymap.set("n", "<leader>fc", builtin.commands, { desc = "Find Commands" })
+
+-- NOTE: rarely used
+-- vim.keymap.set("n", "<leader>fb", builtin.buffer, { "Find Buffers" })
+-- vim.keymap.set("n", "<leader>fC", builtin.colorscheme, { "Find Colorscheme" })
+-- vim.keymap.set("n", "<leader>fM", builtin.man_pages, { "Find Man Pages" })
