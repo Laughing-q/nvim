07c440bd7088d203ecfc5ed0b0d3fd1fed68c807
Laughing-q <1185102784@qq.com>
2024-07-17 11:17:23 +0800

    Update cmp.lua

--- lua/lq/plugins/cmp.lua
+++ lua/lq/plugins/cmp.lua
@@ -16,8 +16,11 @@ return {
 						updateevents = "TextChanged,TextChangedI",
 					})
 
-					require("luasnip/loaders/from_vscode").load({ paths = {} })
-					require("luasnip/loaders/from_vscode").load()
+					require("luasnip/loaders/from_vscode").lazy_load()
+					require("luasnip/loaders/from_vscode").lazy_load({
+						paths = { "~/codes/ultralytics-snippets/" },
+						include = { "python" },
+					})
 				end,
 			},
 
