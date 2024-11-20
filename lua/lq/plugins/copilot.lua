837701c15143118c38affd9983e248936db8b237
Laughing-q <1185102784@qq.com>
2024-11-20 16:54:19 +0800

    comment CopilotChat.nvim

--- lua/lq/plugins/copilot.lua
+++ lua/lq/plugins/copilot.lua
@@ -11,20 +11,20 @@ return {
 			vim.g.copilot_no_tab_map = false
 		end,
 	},
-	{
-		{
-			"CopilotC-Nvim/CopilotChat.nvim",
-			cmd = "CopilotChatOpen",
-			branch = "canary",
-			dependencies = {
-				{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
-				{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
-			},
-			build = "make tiktoken", -- Only on MacOS or Linux
-			opts = {
-				-- See Configuration section for options
-			},
-			-- See Commands section for default commands if you want to lazy load on them
-		},
-	},
+	-- {
+	-- 	{
+	-- 		"CopilotC-Nvim/CopilotChat.nvim",
+	-- 		cmd = "CopilotChatOpen",
+	-- 		branch = "canary",
+	-- 		dependencies = {
+	-- 			{ "github/copilot.vim" }, -- or zbirenbaum/copilot.lua
+	-- 			{ "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
+	-- 		},
+	-- 		build = "make tiktoken", -- Only on MacOS or Linux
+	-- 		opts = {
+	-- 			-- See Configuration section for options
+	-- 		},
+	-- 		-- See Commands section for default commands if you want to lazy load on them
+	-- 	},
+	-- },
 }
