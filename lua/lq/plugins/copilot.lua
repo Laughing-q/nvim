5f07abff439e10111f62f00d1d16f2156de50869
Laughing-q <1185102784@qq.com>
2024-11-21 20:15:41 +0800

    clean up

--- lua/lq/plugins/copilot.lua
+++ lua/lq/plugins/copilot.lua
@@ -30,74 +30,29 @@ return {
 				context = nil, -- Default context to use (can be specified manually in prompt via #).
 				temperature = 0.1, -- GPT result temperature
 
-				question_header = "## User ", -- Header to use for user questions
+				question_header = "# Laughing-q ", -- Header to use for user questions
 				answer_header = "## Copilot ", -- Header to use for AI answers
 				error_header = "## Error ", -- Header to use for errors
 				separator = "───", -- Separator to use in chat
 
 				chat_autocomplete = true, -- Enable chat autocompletion (when disabled, requires manual `mappings.complete` trigger)
-				show_folds = true, -- Shows folds for sections in chat
-				show_help = true, -- Shows help message as virtual lines when waiting for user input
-				auto_follow_cursor = true, -- Auto-follow cursor in chat
-				auto_insert_mode = false, -- Automatically enter insert mode when opening window and on new prompt
+				auto_insert_mode = true, -- Automatically enter insert mode when opening window and on new prompt
 				insert_at_end = false, -- Move cursor to end of buffer when inserting text
 				clear_chat_on_new_prompt = false, -- Clears chat on every new prompt
-				highlight_selection = true, -- Highlight selection in the source buffer when in the chat window
-				highlight_headers = true, -- Highlight headers in chat, disable if using markdown renderers (like render-markdown.nvim)
 
 				history_path = vim.fn.stdpath("data") .. "/copilotchat_history", -- Default path to stored history
 				callback = nil, -- Callback to use when ask response is received
 				no_chat = false, -- Do not write to chat buffer and use chat history (useful for using callback for custom processing)
 
-				-- default prompts
-				prompts = {
-					Explain = {
-						prompt = "> /COPILOT_EXPLAIN\n\nWrite an explanation for the selected code as paragraphs of text.",
-					},
-					Review = {
-						prompt = "> /COPILOT_REVIEW\n\nReview the selected code.",
-						-- see config.lua for implementation
-					},
-					Fix = {
-						prompt = "> /COPILOT_GENERATE\n\nThere is a problem in this code. Rewrite the code to show it with the bug fixed.",
-					},
-					Optimize = {
-						prompt = "> /COPILOT_GENERATE\n\nOptimize the selected code to improve performance and readability.",
-					},
-					Docs = {
-						prompt = "> /COPILOT_GENERATE\n\nPlease add documentation comments to the selected code.",
-					},
-					Tests = {
-						prompt = "> /COPILOT_GENERATE\n\nPlease generate tests for my code.",
-					},
-					Commit = {
-						prompt = "> #git:staged\n\nWrite commit message for the change with commitizen convention. Make sure the title has maximum 50 characters and message is wrapped at 72 characters. Wrap the whole message in code block with language gitcommit.",
-					},
-				},
 				selection = function(source)
 					local select = require("CopilotChat.select")
 					return select.visual(source) or select.line(source)
 				end,
 
-				-- default window options
-				window = {
-					layout = "vertical", -- 'vertical', 'horizontal', 'float', 'replace'
-					width = 0.5, -- fractional width of parent, or absolute width in columns when > 1
-					height = 0.5, -- fractional height of parent, or absolute height in rows when > 1
-					-- Options below only apply to floating windows
-					relative = "editor", -- 'editor', 'win', 'cursor', 'mouse'
-					border = "single", -- 'none', single', 'double', 'rounded', 'solid', 'shadow'
-					row = nil, -- row position of the window, default is centered
-					col = nil, -- column position of the window, default is centered
-					title = "Copilot Chat", -- title of chat window
-					footer = nil, -- footer of chat window
-					zindex = 1, -- determines if window is on top or below other floating windows
-				},
-
 				-- default mappings
 				mappings = {
 					complete = {
-						insert = "<C-k>",
+						insert = "",   -- mapping to empty string disables default completion so it reuse the copilot one
 					},
 					close = {
 						normal = "Q",
