e71975638d089e9a2245025ee012b1e2bca80326
Laughing-q <1185102784@qq.com>
2025-01-06 20:39:48 +0800

    Update yazi.lua

--- lua/lq/plugins/yazi.lua
+++ lua/lq/plugins/yazi.lua
@@ -10,19 +10,19 @@ return {
 				"<cmd>Yazi<cr>",
 				desc = "Open yazi at the current file",
 			},
-			{
-				-- Open in the current working directory
-				"<leader>cw",
-				"<cmd>Yazi cwd<cr>",
-				desc = "Open the file manager in nvim's working directory",
-			},
-			{
-				-- NOTE: this requires a version of yazi that includes
-				-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
-				"<c-up>",
-				"<cmd>Yazi toggle<cr>",
-				desc = "Resume the last yazi session",
-			},
+			-- {
+			-- 	-- Open in the current working directory
+			-- 	"<leader>cw",
+			-- 	"<cmd>Yazi cwd<cr>",
+			-- 	desc = "Open the file manager in nvim's working directory",
+			-- },
+			-- {
+			-- 	-- NOTE: this requires a version of yazi that includes
+			-- 	-- https://github.com/sxyazi/yazi/pull/1305 from 2024-07-18
+			-- 	"<c-up>",
+			-- 	"<cmd>Yazi toggle<cr>",
+			-- 	desc = "Resume the last yazi session",
+			-- },
 		},
 		opts = {
 			-- if you want to open yazi instead of netrw, see below for more info
