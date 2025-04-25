990d84c9e51735f4951a4d130663fddab5b272c7
Laughing-q <1185102784@qq.com>
2025-04-25 16:02:05 +0800

    Update pyright.lua

--- lua/lq/configs/lsp/settings/pyright.lua
+++ lua/lq/configs/lsp/settings/pyright.lua
@@ -1,18 +1,20 @@
 return {
-  -- more settings see https://github.com/microsoft/pyright/blob/main/packages/vscode-pyright/package.json
-  settings = {
-    python = {
-      analysis = {
-        autoSearchPaths = true,
-        autoImportCompletions = false,
-        diagnosticMode = "workspace",
-        useLibraryCodeForTypes = true,
-        typeCheckingMode = "off",
-        logLevel = "Warning",
-        diagnosticSeverityOverrides = {
-          reportUnusedImport = "off",
-        },
-      },
-    },
-  }
+	-- more settings see https://github.com/microsoft/pyright/blob/main/packages/vscode-pyright/package.json
+	settings = {
+		basedpyright = {
+			typeCheckingMode = "recommended",
+			analysis = {
+				autoSearchPaths = true,
+				autoImportCompletions = false,
+				diagnosticMode = "workspace",
+				useLibraryCodeForTypes = true,
+				logLevel = "Warning",
+				diagnosticSeverityOverrides = {
+					reportUnusedImport = "none",
+					reportMissingImports = "none",
+          reportUnknownVariableType = "hint"
+				},
+			},
+		},
+	},
 }
