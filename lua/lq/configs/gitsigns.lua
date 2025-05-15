d0a2c076cfe3752494fa3577fd3722515a25c22e
Laughing-q <1185102784@qq.com>
2025-05-15 15:26:11 +0800

    add git_blame_link

--- lua/lq/configs/gitsigns.lua
+++ lua/lq/configs/gitsigns.lua
@@ -18,31 +18,28 @@ local function get_git_repo()
 	return repo and repo:gsub("%.git$", "") or nil
 end
 
--- Function to generate GitHub commit URL
-local function generate_github_commit_link()
-	-- Get the blame information for the current line
-	-- gitsigns.toggle_current_line_blame(true)
-	local blame_info = vim.b.gitsigns_blame_line_dict
-  -- for key, value in pairs(blame_info) do
-  --     print(key, value)
-  -- end
-
-	if not blame_info or not blame_info.sha then
-		print("No blame information available for current line")
+local async = require("gitsigns.async")
+local git_blame_link = async.create(1, function()
+	local cache = require("gitsigns.cache").cache
+	local bcache = cache[vim.api.nvim_get_current_buf()]
+	if not bcache then
 		return
 	end
 
-	-- Try to get repo dynamically, fallback to hardcoded value
+	local util = require("gitsigns.util")
+	local lnum = vim.api.nvim_win_get_cursor(0)[1]
+
 	local repo = get_git_repo() or "lewis6991/gitsigns.nvim"
-	local commit_url = string.format("https://github.com/%s/commit/%s", repo, blame_info.sha)
+	local result = util.convert_blame_info(assert(bcache:get_blame(lnum, {})))
+  -- for k, v in pairs(result) do
+  --   print(k, v)
+  -- end
+	local commit_url = string.format("https://github.com/%s/commit/%s", repo, result.sha)
 
 	-- Copy to clipboard
 	vim.fn.setreg("+", commit_url)
 	print("Commit URL copied to clipboard: " .. commit_url)
-	-- gitsigns.toggle_current_line_blame(false)
-
-	return commit_url
-end
+end)
 
 local opts = {
 	signs = {
@@ -80,7 +77,7 @@ local opts = {
 	sign_priority = 6,
 	update_debounce = 200,
 	status_formatter = nil, -- Use default
-	current_line_blame = true, -- Enable this for easier access to blame info
+	current_line_blame = false, -- Enable this for easier access to blame info
 
 	on_attach = function(bufnr)
 		local function map(mode, l, r, opts)
@@ -132,7 +129,7 @@ local opts = {
 
 		-- Add custom keymap for generating GitHub links
 		vim.keymap.set("n", "<leader>gL", function()
-			generate_github_commit_link()
+			git_blame_link()
 		end, { buffer = bufnr, desc = "Generate GitHub link for current line's commit" })
 
 		-- -- Optional: Add keymap to open the link in browser if you have a plugin for it
