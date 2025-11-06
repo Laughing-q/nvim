ce516a359e3086ab696efd9f684f33df7361d660
Laughing-q <1185102784@qq.com>
2025-11-06 22:51:29 +0800

    Update plugins/harpoon.lua

--- lua/lq/plugins/harpoon.lua
+++ lua/lq/plugins/harpoon.lua
@@ -1,31 +1,39 @@
 return {
 	{
 		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
 		event = "VimEnter",
+		requires = { { "nvim-lua/plenary.nvim" } },
 		config = function()
-			local mark = require("harpoon.mark")
-			local ui = require("harpoon.ui")
+			local harpoon = require("harpoon")
 			local map = vim.keymap.set
-			map("n", "<leader>a", mark.add_file)
-			map("n", "<C-k>", ui.toggle_quick_menu)
 
-			map("n", "<A-l>", function()
-				ui.nav_next()
+			harpoon:setup()
+			map("n", "<leader>a", function()
+				harpoon:list():add()
 			end)
-			map("n", "<A-j>", function()
-				ui.nav_prev()
+			map("n", "<C-k>", function()
+				harpoon.ui:toggle_quick_menu(harpoon:list())
 			end)
+
 			map("n", "<A-1>", function()
-				ui.nav_file(1)
+				harpoon:list():select(1)
 			end)
 			map("n", "<A-2>", function()
-				ui.nav_file(2)
+				harpoon:list():select(2)
 			end)
 			map("n", "<A-3>", function()
-				ui.nav_file(3)
+				harpoon:list():select(3)
 			end)
 			map("n", "<A-4>", function()
-				ui.nav_file(4)
+				harpoon:list():select(4)
+			end)
+
+			map("n", "<A-j>", function()
+				harpoon:list():prev()
+			end)
+			map("n", "<A-l>", function()
+				harpoon:list():next()
 			end)
 		end,
 	},
