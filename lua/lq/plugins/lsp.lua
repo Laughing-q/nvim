a03dc11f712ffae24261599e2652d9ae111ba24f
Laughing-q <1185102784@qq.com>
2025-12-26 17:19:10 +0800

    add fixed commit for lspconfig plugin for now

--- lua/lq/plugins/lsp.lua
+++ lua/lq/plugins/lsp.lua
@@ -3,6 +3,7 @@ return {
 		"neovim/nvim-lspconfig",
 		dependencies = { "mason.nvim", config = true },
 		event = "VeryLazy",
+    commit = "5bfcc89fd155b4ffc02d18ab3b7d19c2d4e246a7",
 		config = function()
 			require("lq.configs.lsp.lspconfig")
 			vim.cmd("silent! do FileType")
