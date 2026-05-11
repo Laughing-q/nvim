c53e803ed6ae8accf485287c3e5a31d7cddb34c1
Laughing-q <1185102784@qq.com>
2026-05-11 17:40:11 +0800

    add git modified files to harpoon

--- lua/lq/plugins/harpoon.lua
+++ lua/lq/plugins/harpoon.lua
@@ -9,6 +9,32 @@ return {
 			map("n", "<leader>a", mark.add_file)
 			map("n", "<C-k>", ui.toggle_quick_menu)
 
+			map("n", "<leader>A", function()
+				-- Check if in git repo
+				local git_root = vim.fn.systemlist("git rev-parse --show-toplevel")
+				if vim.v.shell_error ~= 0 or #git_root == 0 then
+					vim.notify("Not in a git repository", vim.log.levels.WARN)
+					return
+				end
+				git_root = git_root[1]
+
+				-- Clear existing marks
+				mark.clear_all()
+
+				-- Add git modified/untracked files
+				local files = vim.fn.systemlist("git ls-files --modified --others --exclude-standard")
+				local count = 0
+				for _, file in ipairs(files) do
+					local abs_path = git_root .. "/" .. file
+					if vim.fn.filereadable(abs_path) == 1 then
+						mark.add_file(abs_path)
+						count = count + 1
+					end
+				end
+
+				vim.notify("Added " .. count .. " modified file(s) to harpoon", vim.log.levels.INFO)
+			end, { desc = "Add git modified files to harpoon" })
+
 			map("n", "<A-l>", function()
 				ui.nav_next()
 			end)
